b4b1e303c470397e2c2d725a14abd7f8431560f6
WinZ606 <2005bnm@gmail.com>
2024-11-07 09:05:42 +0100

    5 feladat2

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import feladatok
 
 feladatok.feladat1()
 feladatok.feladat2()
-szoveg:str = "Mivan"
-szam:int = 2
+szoveg:str = "Ez az én dumám"
+szam:int = 4
 feladatok.feladat3(szoveg,szam)
 nevekszam = feladatok.feladat4()
 print(f"A felhasználó {nevekszam-1}db nevet adott meg!")
